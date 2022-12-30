fd2d282577d44d776462bc7892c3d0c154e43adb
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2022-12-29 19:16:54 -0500

    abusive stuff

---
+++ SysBot.Pokemon/SV/BotTrade/BadUserList.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace SysBot.Pokemon
+{
+    /// <summary>
+    /// A list of users that have or are actively trying to get bots softbanned by leaving trades early (2+ attempts).
+    /// There is currently no mitigation for being softbanned so we just leave the trade if we see one of these traders.
+    /// There is probably a more elegant way to do this, but this is the best we have while the game is new.
+    /// </summary>
+    public static class BadUserList
+    {
+        public static IReadOnlyList<ulong> Users { get; } = new ulong[]
+        {
+            17966876822808517034,
+            
+        };
+    }
+}
