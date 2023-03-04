4f5e367cd5ff435014918855800389489a255625
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2023-03-03 23:16:29 -0500

    Update PokeDataOffsetsSV.cs

--- SysBot.Pokemon/SV/Vision/PokeDataOffsetsSV.cs
+++ SysBot.Pokemon/SV/Vision/PokeDataOffsetsSV.cs
@@ -17,9 +17,14 @@ namespace SysBot.Pokemon
         public static IReadOnlyList<long> LinkTradePartnerNameSlot2Pointer { get; } = new long[] { 0x44CCBB0, 0x28, 0xE0, 0x00 };
         public static IReadOnlyList<long> LinkTradePartnerNIDPointer { get; } = new long[] { 0x44C7730, 0xF8, 0x08 };
 
-        public static IReadOnlyList<long> KeyboardBufferPointer { get; } = new long[] { 0x437F908, 0x30, 0x00 }; 
+        public static IReadOnlyList<long> OverworldPointer { get; } = new long[] { 0x45187D8, 0x00, 0x388, 0x3C0, 0x00, 0x1A1C };
+        public static IReadOnlyList<long> IsSearchingPointer { get; } = new long[] { 0x44CC9B8, 0x58 }; // 0 no search, 1 search, 2 unknown (still searching)
+        public static IReadOnlyList<long> ConnectionPointer { get; } = new long[] { 0x44CCBC0, 0x10 }; // 0 not connected, 1 connected, 2 adhoc
 
-        public static IReadOnlyList<long> PokePortalPointer { get; } = new long[] { 0x43F3538, 0x00, 0x3C0, 0x3C0, 0x598 };
+        public static IReadOnlyList<long> KeyboardBufferPointer { get; } = new long[] { 0x44A46B8, 0x30, 0x00 };
+
+
+        public static IReadOnlyList<long> PokePortalPointer = new long[] { 0x45187D8, 0x00, 0x3C0, 0x3C0, 0x598 };
 
         public const int BoxFormatSlotSize = 0x158;
         public const int TradeFormatSlotSize = 0x148;
